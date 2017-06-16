b055dc035f7e32edaa74776f263cb368cace1e40
evelh <liu8824@qq.com>
2017-06-16 13:09:55 +0800

    添加客户告警类

---
+++ EMT.DoneNOW.Core/CRM/crm_account_alert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EMT.DoneNOW.Core
+{
+    [Table("crm_account_alert")]
+    [Serializable]
+    public partial class crm_account_alert
+    {
+
+        [Key]
+        [Column("id")]
+        public Int64 id { get; set; }
+        [Column("create_time")]
+        public Int64? create_time { get; set; }
+        [Column("create_by_id")]
+        public Int64? create_by_id { get; set; }
+        [Column("update_time")]
+        public Int64? update_time { get; set; }
+        [Column("update_by_id")]
+        public Int64? update_by_id { get; set; }
+        [Column("delete_time")]
+        public Int64? delete_time { get; set; }
+        [Column("delete_by_id")]
+        public Int64? delete_by_id { get; set; }
+        [Column("alert_text")]
+        public String alert_text { get; set; }
+        [Column("alert_type_id")]
+        public String alert_type_id { get; set; }
+        [Column("account_id")]
+        public Int64? account_id { get; set; }
+
+
+    }
+}
---
+++ EMT.DoneNOW.DAL/CRM/crm_account_alert_dal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMT.DoneNOW.Core;
+namespace EMT.DoneNOW.DAL
+{
+    public class crm_account_alert_dal : BaseDAL<crm_account_alert>
+    {
+        /// <summary>
+        /// 根据客户id查找
+        /// </summary>
+        /// <returns></returns>
+        public List<crm_account_alert> FindByAccount(long account_id)
+        {
+            return FindListBySql("SELECT * FROM crm_account_alert WHERE account_id=" + account_id);
+        }
+    }
+}
