5320e1f68fddbcdb93701e2d6ed8607df65c8242
evelh <liu8824@qq.com>
2017-07-05 13:13:54 +0800

    自定义字段功能

--- EMT.DoneNOW.DTO/UserDefinedFieldDto.cs
+++ EMT.DoneNOW.DTO/UserDefinedFieldDto.cs
@@ -20,16 +20,20 @@ namespace EMT.DoneNOW.DTO
         [DataMember]
         public string name;         // 字段显示名
         [DataMember]
-        public int cate;            // 字段类型
+        public int cate;            // 对象类型(客户、联系人等)
+        [DataMember]
+        public int data_type;       // 字段类型(单行文本、列表等)
+        [DataMember]
+        public int? display_format; // 显示样式(单行、多行等)
         [DataMember]
         public SByte required;      // 是否必填
         [DataMember]
-        public int decimal_length;  // 小数位数
+        public int? decimal_length; // 小数位数
         [DataMember]
         public string default_value;// 默认值
         [DataMember]
         public string description;  // 字段描述
         [DataMember]
-        public object value_list;   // 字段类型为列表时的列表键值
+        public List<DictionaryEntryDto> value_list;   // 字段类型为列表时的列表键值
     }
 }
