943df0d6a3493b88f9e9e0f4142c7347f7cf33f3
DESKTOP-JLTO318\zhufei <zhufei_dsjt@shdsjt.cn>
2017-08-09 14:30:20 +0800

    客户Ajax获取联系人更改

--- EMT.DoneNOW.Web/Tools/CompanyAjax.ashx.cs
+++ EMT.DoneNOW.Web/Tools/CompanyAjax.ashx.cs
@@ -33,7 +33,8 @@ namespace EMT.DoneNOW.Web
                     break;
                 case "contact":    // 通过客户id去获取联系人列表
                     var account_id = context.Request.QueryString["account_id"];
-                    GetCompanyContact(context, account_id);
+                    var isUserParentContact = context.Request.QueryString["userParentContact"];//userParentContact
+                    GetCompanyContact(context, account_id, isUserParentContact);
                     break;
                 case "companyPhone":
                     var id = context.Request.QueryString["account_id"];
@@ -91,20 +92,38 @@ namespace EMT.DoneNOW.Web
         /// </summary>
         /// <param name="context"></param>
         /// <param name="account_id"></param>
-        private void GetCompanyContact(HttpContext context, string account_id)
+        private void GetCompanyContact(HttpContext context, string account_id,string parentContact="")
         {
             try
             {
+                StringBuilder contacts = new StringBuilder("<option value='0'>     </option>");
                 var contactList = new ContactBLL().GetContactByCompany(Convert.ToInt64(account_id));
                 if (contactList != null && contactList.Count > 0)
                 {
-                    StringBuilder contacts = new StringBuilder("<option value='0'>     </option>");
                     foreach (var contact in contactList)
                     {
                         contacts.Append("<option value='" + contact.id + "'>" + contact.name + "</option>");
                     }
-                    context.Response.Write(contacts);
                 }
+                if ( !string.IsNullOrEmpty(parentContact))
+                {
+                    var account = new CompanyBLL().GetCompany(Convert.ToInt64(account_id));
+                    if (account.parent_id != null)
+                    {
+                        var parentContactList = new ContactBLL().GetContactByCompany((long)account.parent_id);
+                        if (parentContactList != null && parentContactList.Count > 0)
+                        {
+                            contacts.Append("<option value='0'> ------- </option>");
+                            foreach (var contact in parentContactList)
+                            {
+                                contacts.Append("<option value='" + contact.id + "'>" + contact.name + "</option>");
+                            }
+                        }
+                    }
+                }
+             
+                context.Response.Write(contacts);
+                return;
             }
             catch (Exception)
             {
